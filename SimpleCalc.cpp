cef4d88f7a10d3d82b900b7d09e8e3fe422e9f46
iaudhslosontawpbebe <117110719+iaudhslosontawpbebe@users.noreply.github.com>
2024-09-19 18:38:25 +0800

    Create SimpleCalc.cpp

---
+++ SimpleCalc.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void calculator() {
+    double num1, num2;
+    char op;
+
+    cout << "Enter first number: ";
+    cin >> num1;
+    cout << "Enter operator (+, -, *, /): ";
+    cin >> op;
+    cout << "Enter second number: ";
+    cin >> num2;
+
+    switch(op) {
+        case '+': 
+            cout << "Result: " << num1 + num2 << endl;
+            break;
+        case '-': 
+            cout << "Result: " << num1 - num2 << endl;
+            break;
+        case '*': 
+            cout << "Result: " << num1 * num2 << endl;
+            break;
+        case '/': 
+            if(num2 != 0)
+                cout << "Result: " << num1 / num2 << endl;
+            else
+                cout << "Error: Division by zero" << endl;
+            break;
+        default:
+            cout << "Error: Invalid operator" << endl;
+            break;
+    }
+}
+
+int main() {
+    calculator();
+    return 0;
+}
